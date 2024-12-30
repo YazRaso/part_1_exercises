ab339611eda0d57a935b16f2071fa9ee6bfaf843
YazRaso <seyedyazdanrasoulzadeh@gmail.com>
2024-12-31 02:43:22 +0400

    Completed part 1 exercises

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,52 @@
-const Header = (props) => {
-    return (   
-      <h1>{props.course}</h1>
-    )
+import { useState } from 'react'
+const MostVotes = (props, anec) => {
+  const mostVotesIndex = props.reduce((maxIdx, current, idx, array) => current > array[maxIdx] ? idx : maxIdx, 0)
+  return(
+  <div>
+  <h1>Anecdote with the most votes</h1>
+  <p>{anec[mostVotesIndex]}</p>
+  <p>has {props[mostVotesIndex]}</p>
+  </div>
+  )
+}
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
+  console.log(points)
+  const handleVote = () => {
+    const newPoints = [...points]
+    newPoints[selected] += 1
+    setPoints(newPoints)
   }
-  const Part = (props) => {
-    return (
-      <p>{props.props.name} {props.props.exercises}</p>
-    )
+  const handleNext = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
   }
-  const Content = (props) => {
-    return (
+
+  return (
     <div>
-      <Part props={props.props[0]}/>
-      <Part props={props.props[1]}/>
-      <Part props={props.props[2]}/>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <br></br>
+      has {points[selected]} votes
+      <br>
+      </br>
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleNext}>Next anecdote</button> 
+      {MostVotes(points, anecdotes)}
     </div>
-    )
-  }
-
-  const Total = (props) => {
-    return(
-      <p>Number of exercises {props.props.reduce((accul, current) => accul + current.exercises, 0)}</p>
-    )
-  }
-
-
-
-  const App = () => {
-    const course = {
-      name: 'Half Stack application development',
-      part: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7
-        },
-        {
-          name: 'State of a component',
-          exercises: 14
-        }
-      ]
-    }
-    console.log(course.part)
-    return (
-      <div>
-        <Header course={course.name} />
-        <Content props={course.part} />
-        <Total props={course.part} />
-      </div>
-    )
-  }
+  )
+}
 
 export default App
